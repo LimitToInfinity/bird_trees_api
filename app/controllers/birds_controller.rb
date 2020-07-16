e87b60301f3150c905f4dc58f672d426a4c039d5
LimitToInfinity <21stLimitless@gmail.com>
2020-07-16 11:50:44 -0600

    Adds index, show, and create actions for Bird.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,2 +1,22 @@
 class BirdsController < ApplicationController
+
+  def index
+    @birds = Bird.all
+    render json: @birds
+  end
+
+  def show
+    @bird = Bird.find(params[:id])
+    render json: @bird
+  end
+
+  def create
+    byebug
+    @bird = Bird.create(
+      name: params[:name],
+      species: params[:species]
+    )
+    render json: @bird, status: :created
+  end
+
 end
